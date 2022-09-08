03b6fd1012586ea2253051769b5f19e633812a85
alaasassii <alaasassijs@gmail.com>
2022-09-08 22:06:56 +0100

    adding things

---
+++ src/routes/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='w-[200px] bg-rose-500 h-1 absolute top-[50%] left-[50%]  translate-x-[-50%]'></div>
+  )
+}
+
+export default SignUp
