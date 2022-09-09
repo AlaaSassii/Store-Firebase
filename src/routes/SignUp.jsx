2da474864e35b9c67153b25b7006038ed9a91e1f
alaasassii <alaasassijs@gmail.com>
2022-09-09 21:36:08 +0100

    adding signUp

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { BsFacebook  ,FaGithub , BsTwitter} from 'react-icons/fa';
 const SignUp = () => {
   return (
-    <div className='w-[200px] bg-rose-500 h-1 absolute top-[50%] left-[50%]  translate-x-[-50%]'></div>
+    <div className='w-[80%] bg-gray-100 h-1 absolute top-[50%] left-[50%] translate-x-[-50%]'>
+      <div>
+        <h4>Sign in to your account</h4>
+        <p>or <a>Create an account</a></p>
+        <p>Sign in with</p>
+        <div className="links">
+          <div><BsFacebook/></div>
+          <div><FaGithub/></div>
+          <div><BsTwitter/></div>
+        </div>
+        <button></button>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
   )
 }
 
