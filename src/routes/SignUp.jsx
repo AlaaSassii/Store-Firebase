9681c2c2bae0c29013481395a755dea6bfb1ab92
alaasassii <alaasassijs@gmail.com>
2022-09-09 22:58:19 +0100

    adding really signUp

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,40 +1,31 @@
 import React from 'react'
-import { BsFacebook  , BsTwitter} from 'react-icons/bs';
-import {FaGithub } from 'react-icons/fa';
+
 const SignUp = () => {
   return (
-    <div className=' w-[80%] md:w-[60%] bg-gray-100 flex absolute top-[30%] left-[50%] translate-x-[-50%] rounded'>
-      <div className='md:basis-[50%] w-[100%] md:p-[20px] p-[30px]'>
-        <h4 className='text-[21px] font-bold'>Sign in to your account</h4>
-        <p>or <a className='text-blue-400'>Create an account</a></p>
-        <p className='text-sm'>Sign in with</p>
-        <div className="flex content-between w-[100%]">
-          <div className='border-solid w-[33%] 	bg-white h-[33px] rounded relative p-2  m-2 flex content-center place-items-center'><div className='absolute left-[50%] translate-x-[-50%]'><BsFacebook className='text-[20px]'/></div></div>
-          <div className='border-solid w-[33%] 	bg-white h-[33px] rounded relative p-2  m-2 flex content-center place-items-center'><div className='absolute left-[50%] translate-x-[-50%]'><FaGithub className='text-[20px]'/></div></div>
-          <div className='border-solid w-[33%] 	bg-white h-[33px] rounded relative p-2  m-2 flex content-center place-items-center'><div className='absolute left-[50%] translate-x-[-50%]'><BsTwitter className='text-[20px]'/></div></div>
-        </div>
-        {/*   */}
-        <label htmlFor="" className='text-sm'>
-          Email address
-          <input type="text" className='block p-1 w-[100%] rounded m-1' />
+    <div className='absolute top-[30%] left-[50%] md:w-[29%] w-[80%] translate-x-[-50%] bg-slate-200 p-5 rounded'>
+        <h3 className='text-[25px] font-semibold  text-center'>Create an account</h3>
+        <p className='mt-3  text-center'>Or <a className='text-blue-400 '>Login</a></p>
+        <label className='block w-[80%] m-auto '> 
+            Full name 
+            <input className='block w-[100%] mt-[5px] p-1 rounded' />
         </label>
-        <label htmlFor="" className='text-sm'>
-          password
-          <input type="password" className='block p-1 w-[100%] rounded m-1' />
+        <label className='block  w-[80%] m-auto' > 
+            Email address
+            <input className='block w-[100%] mt-[5px] p-1 rounded '/>
         </label>
-        <div className='flex content-between bg-red-400' >
-          <label htmlFor="" className='text-sm'>
-          <input type='checkbox' /> 
-            Remember me
-          </label>
-          <span>{'  '}</span>
-          <p className='text-sm text-blue-400 '>Forgot your password </p>
-        </div>
-        <button className='w-[80%] ml-5 p-1 text-white size-[20px] font-bold mt-3 bg-blue-400 rounded m-auto '>Sign in</button>
-      </div>
-      <div className='md:w-[70%] hidden md:block'>
-        <img className='object-cover' src="https://www.incimages.com/uploaded_files/image/1920x1080/getty_655998316_2000149920009280219_363765.jpg" alt="" />
-      </div>
+        <label className='block  w-[80%] m-auto' > 
+            Password
+            <input className='block w-[100%] mt-[5px] p-1 rounded  '/>
+        </label>
+        <label  className='block  w-[80%] m-auto'> 
+            Profit Image 
+            <input className='block w-[100%] mt-[5px] p-1 rounded  '/>
+        </label>
+        <label className='block  w-[80%] m-auto ' > 
+            Birth Date
+            <input className='block w-[100%] mt-[5px] p-1 rounded  '/>
+        </label>
+        <button className='bg-blue-400 w-[60%] p-1 mx-[100px] mt-3 rounded'>Sign Up</button>
     </div>
   )
 }
