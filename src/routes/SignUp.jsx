436fa090da1f03fef42e1f3d92646c8566f57178
alaasassii <alaasassijs@gmail.com>
2022-09-09 23:17:26 +0100

    adding really signUp

--- src/routes/SignUp.jsx
+++ src/routes/SignUp.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React ,{useState , useEffect} from 'react'
 
 const SignUp = () => {
+    const [preview, setPreview] = useState();
+    const [image, setImage] = useState();
+    const fileInputRef = useRef();
+    useEffect(() => {
+        
+        if (image) {
+          const reader = new FileReader();
+          reader.onloadend = () => {
+            setPreview(reader.result );
+        };
+        reader.readAsDataURL(image);
+        dispatch({type: 'FILE',payload:preview})
+        } else {
+          setPreview(null);
+        }
+      }, [image,preview]);
   return (
     <div className='absolute top-[30%] left-[50%] md:w-[29%] w-[80%] translate-x-[-50%] bg-slate-200 p-5 rounded'>
         <h3 className='text-[25px] font-semibold  text-center'>Create an account</h3>
         <p className='mt-3  text-center'>Or <a className='text-blue-400 '>Login</a></p>
         <label className='block w-[80%] m-auto '> 
             Full name 
-            <input className='block w-[100%] mt-[5px] p-1 rounded' />
+            <input type="text" className='block w-[100%] mt-[5px] p-1 rounded' />
         </label>
         <label className='block  w-[80%] m-auto' > 
             Email address
-            <input className='block w-[100%] mt-[5px] p-1 rounded '/>
+            <input type='email' className='block w-[100%] mt-[5px] p-1 rounded '/>
         </label>
         <label className='block  w-[80%] m-auto' > 
             Password
-            <input className='block w-[100%] mt-[5px] p-1 rounded  '/>
+            <input type="password" className='block w-[100%] mt-[5px] p-1 rounded  '/>
         </label>
+        {/* FILE */}
         <label  className='block  w-[80%] m-auto'> 
             Profit Image 
-            <input className='block w-[100%] mt-[5px] p-1 rounded  '/>
+            <input type='file' className='block w-[100%] mt-[5px] p-1 rounded  '/>
         </label>
+        {/* File */}
         <label className='block  w-[80%] m-auto ' > 
             Birth Date
-            <input className='block w-[100%] mt-[5px] p-1 rounded  '/>
+            <input type="date" className='block w-[100%] mt-[5px] p-1 rounded  '/>
         </label>
-        <button className='bg-blue-400 w-[60%] p-1 mx-[100px] mt-3 rounded'>Sign Up</button>
+        <button className='bg-blue-400 w-[60%] p-1 mx-auto mt-3 rounded block '>Sign Up</button>
     </div>
   )
 }
